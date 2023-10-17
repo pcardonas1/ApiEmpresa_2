992dbc749da84a49e089bec265df8a887bc311ff
pcardonas1 <98655213+pcardonas1@users.noreply.github.com>
2023-10-16 22:39:24 -0600

    Initial commit

---
+++ ApiEmpresa/Models/Clientes.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiEmpresa.Models;
+public class Clientes{
+    [Key]
+    public Int32 Id_Cliente { get; set; }
+    public string? nombres { get; set; }
+    public string? apellidos { get; set; }
+    public string? direccion { get; set; }
+    public string? telefono { get; set; }
+    public DateTime? fecha_nacimiento { get; set; }
+
+
+
+
+}
---
+++ ApiEmpresa/Models/Conexiones.cs
@@ -0,0 +1,13 @@
+using System.Runtime.InteropServices;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiEmpresa.Models;
+
+public class Conexiones : DbContext {
+     public Conexiones(DbContextOptions<Conexiones> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Clientes> Clientes { get; set; } = null!;
+}
